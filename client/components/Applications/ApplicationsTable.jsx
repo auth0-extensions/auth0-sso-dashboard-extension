433e2c8301aa50a10314351c6784799555c8f2b7
Oleksandr Zarubin <oleksandr.zarubin@ua.akvelon.com>
2016-08-09 10:17:03 +0300

    cards

--- client/components/Applications/ApplicationsTable.jsx
+++ client/components/Applications/ApplicationsTable.jsx
@@ -1,6 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import { Link } from 'react-router';
 
-import { TableActionCell, Table, TableCell, TableRouteCell, TableBody, TableTextCell, TableHeader, TableColumn, TableRow } from '../Dashboard';
+import {
+  TableActionCell,
+  Table,
+  TableCell,
+  TableRouteCell,
+  TableBody,
+  TableTextCell,
+  TableHeader,
+  TableColumn,
+  TableRow
+} from '../Dashboard';
 
 export default class ApplicationsTable extends Component {
   static propTypes = {
@@ -14,44 +25,32 @@ export default class ApplicationsTable extends Component {
   }
 
   render() {
-    const { applications, renderActions } = this.props;
+    const {applications, renderActions} = this.props;
     return (
-      <Table>
-        <TableHeader>
-          <TableColumn width="15%">Logo</TableColumn>
-          <TableColumn width="50%">Name</TableColumn>
-          <TableColumn width="20">SSO-Type</TableColumn>
-          <TableColumn width="15">Status</TableColumn>
-        </TableHeader>
-        <TableBody>
+      <div id="cards-component" class="tab-pane">
         {applications.map((application, index) => {
-            if(application.client_metadata) {
-                return (
-                    <TableRow key={index}>
-                        <TableCell>
-                            <img className="img-circle" src={ application.client_metadata['sso-dashboard-logo'] } alt={ application.name || application.client_id} width="32" />
-                        </TableCell>
-                        <TableRouteCell route={`/applications/${application.client_id}`}>{ application.name || application.client_id }</TableRouteCell>
-                        <TableTextCell>{ application.client_metadata['sso-dashboard-type'] }</TableTextCell>
-                        <TableTextCell>{ application.client_metadata['sso-dashboard-enabled']=='1'?'Yes':'No' }</TableTextCell>
-                    </TableRow>
-                );
-            } else {
-                return (
-                    <TableRow key={index}>
-                        <TableCell>
-                            <img className="img-circle" src="https://rawgit.com/auth0-extensions/auth0-delegated-administration-extension/master/docs/theme/fabrikam.svg" alt={ application.name || application.client_id} width="32" />
-                        </TableCell>
-                        <TableRouteCell route={`/applications/${application.client_id}`}>{ application.name || application.client_id }</TableRouteCell>
-                        <TableTextCell>None</TableTextCell>
-                        <TableTextCell>None</TableTextCell>
-                    </TableRow>
-                );
-            }
+          const logo = (application.client_metadata) ? application.client_metadata['sso-dashboard-logo'] : 'https://rawgit.com/auth0-extensions/auth0-delegated-administration-extension/master/docs/theme/fabrikam.svg';
+          const type = (application.client_metadata) ? application.client_metadata['sso-dashboard-type'] : 'None';
+          const enabled = (application.client_metadata && application.client_metadata['sso-dashboard-enabled'] == '1') ? 'Enabled' : 'Disabled';
+          const name = application.name || application.client_id;
 
+          return (
+            <div className="col-xs-12 col-md-4" key={ application.client_id }>
+              <article className="card-docs">
+                <img className="img-circle" src={ logo } alt={ name } width="32"/>
+                <h2 className="card-docs-title">{ name }</h2>
+                <div className="card-docs-links">
+                  <span href="" className="card-docs-link">{ type }</span>
+                  <span href="" className="card-docs-link">{ enabled }</span>
+                </div>
+                <Link className="card-docs-more" to={`/applications/${application.client_id}`}>
+                  Edit
+                </Link>
+              </article>
+            </div>
+          );
         })}
-        </TableBody>
-      </Table>
+      </div>
     );
   }
 }
