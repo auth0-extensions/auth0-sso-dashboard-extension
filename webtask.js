7dd11caee6d71375bc9fe51fa523a422524fce7e
Oleksandr Zarubin <oleksandr.zarubin@ua.akvelon.com>
2018-01-10 13:51:48 +0200

    new tools for non-i pipeline

--- webtask.js
+++ webtask.js
@@ -1,19 +1,8 @@
-const url = require('url');
 const tools = require('auth0-extension-express-tools');
 
 const expressApp = require('./server');
 const config = require('./server/lib/config');
 const logger = require('./server/lib/logger');
-const webtask = require('./server/lib/webtask');
-
-tools.urlHelpers.getBaseUrl = (req) => {
-  const originalUrl = url.parse(req.originalUrl || '').pathname || '';
-  return url.format({
-    protocol: 'https',
-    host: req.headers.host,
-    pathname: originalUrl.replace(req.path, '').replace(/\/$/g, '')
-  });
-};
 
 const createServer = tools.createServer((cfg, storage) => {
   logger.info('Starting SSO Dashboard Extension - Version:', process.env.CLIENT_VERSION);
@@ -22,6 +11,6 @@ const createServer = tools.createServer((cfg, storage) => {
 
 
 module.exports = (context, req, res) => {
-  config.setValue('PUBLIC_WT_URL', webtask.getUrl(req));
+  config.setValue('PUBLIC_WT_URL', tools.urlHelpers.getWebtaskUrl(req));
   createServer(context, req, res);
 };
