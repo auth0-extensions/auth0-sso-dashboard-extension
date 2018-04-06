249f2beec20991e8a117d804084b08c9ef73e613
Oleksandr Zarubin <oleksandr.zarubin@ua.akvelon.com>
2018-04-06 16:42:23 +0300

    Multipart request for clients and connections

--- server/routes/connections.js
+++ server/routes/connections.js
@@ -1,11 +1,12 @@
 import _ from 'lodash';
 import { Router } from 'express';
 import { requireScope } from '../lib/middlewares';
+import multipartRequest from '../lib/multipartRequest';
 
 export default (auth0) => {
   const api = Router();
   api.get('/', auth0, requireScope('manage:applications'), (req, res, next) => {
-    req.auth0.connections.getAll({ fields: 'name' })
+    multipartRequest(req.auth0, 'connections', { fields: 'name' })
       .then(connections => _.chain(connections)
         .sortBy((conn) => conn.name.toLowerCase())
         .value())
