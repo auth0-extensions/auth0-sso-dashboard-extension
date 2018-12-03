1aada4c507b7eab7b2696835e21fd7547dd8a8ec
Alexander Zarubin <nonexist.void@gmail.com>
2018-12-03 17:01:34 +0200

    linting

--- server/lib/multipartRequest.js
+++ server/lib/multipartRequest.js
@@ -1,7 +1,7 @@
 import Promise from 'bluebird';
 import { ArgumentError } from 'auth0-extension-tools';
 
-export default function(client, entity, opts = {}, perPage = 100, concurrency = 5) {
+export default function (client, entity, opts = {}, perPage = 100, concurrency = 5) {
   if (client === null || client === undefined) {
     throw new ArgumentError('Must provide a auth0 client object.');
   }
@@ -41,7 +41,7 @@ export default function(client, entity, opts = {}, perPage = 100, concurrency = 
         }
 
         const pages = [];
-        for (let i=1; i<=pageCount; i++) {
+        for (let i = 1; i <= pageCount; i++) {
           pages.push(i);
         }
 
