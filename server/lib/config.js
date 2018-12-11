f5941f93fdab34d70aa3a56832faeeb71f9042fe
zxan1285 <nonexist.void@gmail.com>
2018-12-11 17:50:47 +0200

    Exposing ALLOW_AUTHZ (#39)
    * ALLOW_AUTHZ secret added to the webtask.json
    
    * code comment and description fix
    
    * linting

--- server/lib/config.js
+++ server/lib/config.js
@@ -1,16 +1,26 @@
 const settings = { };
 let currentProvider = null;
 
+
+const boolify = (item) => {
+  // as we're trying to "boolify" every secret, we need to make sure we're processing only boolean secrets
+  if (item === 'true' || item === 'false') {
+    return item === 'true';
+  }
+
+  return item;
+};
+
 const config = (key) => {
   if (settings && settings[key]) {
-    return settings[key];
+    return boolify(settings[key]);
   }
 
   if (!currentProvider) {
     throw new Error('A configuration provider has not been set');
   }
 
-  return currentProvider(key);
+  return boolify(currentProvider(key));
 };
 
 config.setProvider = (providerFunction) => {
