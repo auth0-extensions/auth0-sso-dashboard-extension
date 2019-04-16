9f10f8f167ce11d068c964eedb05071d110f5415
Alexander Zarubin <nonexist.void@gmail.com>
2019-04-16 10:37:15 +0300

    removed outdated patched functions, using latest express-tools, fixed public url

--- webtask.js
+++ webtask.js
@@ -1,27 +1,19 @@
-const url = require('url');
 const tools = require('auth0-extension-express-tools');
 
 const expressApp = require('./server').default;
 const config = require('./server/lib/config');
 const logger = require('./server/lib/logger');
-const webtask = require('./server/lib/webtask');
-
-tools.urlHelpers.getBaseUrl = (req) => {
-  const originalUrl = url.parse(req.originalUrl || '').pathname || '';
-  return url.format({
-    protocol: 'https',
-    host: req.headers.host,
-    pathname: originalUrl.replace(req.path, '').replace(/\/$/g, '')
-  });
-};
 
 const createServer = tools.createServer((cfg, storage) => {
   logger.info('Starting SSO Dashboard Extension - Version:', process.env.CLIENT_VERSION);
   return expressApp(cfg, storage);
 });
 
-
 module.exports = (context, req, res) => {
-  config.setValue('PUBLIC_WT_URL', webtask.getUrl(req));
+  const publicUrl = (req.x_wt && req.x_wt.ectx && req.x_wt.ectx.PUBLIC_WT_URL) || false;
+  if (!publicUrl) {
+    config.setValue('PUBLIC_WT_URL', tools.urlHelpers.getWebtaskUrl(req));
+  }
+
   createServer(context, req, res);
 };
